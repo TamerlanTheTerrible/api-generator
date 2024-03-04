8bc4da9e52495737635e250c15a4471eaed4fa51
t.ismoilov <t.ismoilov@paymo.uz>
2024-03-04 14:26:55 +0500

    first commit

---
+++ src/main/java/uz/atmos/gaf/GAFApplication.java
@@ -0,0 +1,12 @@
+package uz.atmos.gaf;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class GAFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GAFApplication.class, args);
+	}
+}
