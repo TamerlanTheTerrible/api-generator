82f9f300a1e69b7fe2c0c7d94db9fbe9026b12e1
t.ismoilov <t.ismoilov@paymo.uz>
2024-03-18 17:27:24 +0500

    start moving to freemaker #6

--- src/main/java/uz/atmos/gaf/client/source_generator/impl/GrpcClientFMTemplateGenerator.java
+++ src/main/java/uz/atmos/gaf/client/source_generator/impl/GrpcClientFMTemplateGenerator.java
@@ -10,7 +10,7 @@ import javax.lang.model.element.Element;
  * Created by Temurbek Ismoilov on 12/03/24.
  */
 
-public class ThriftClientGenerator implements ClientGenerator {
+public class GrpcClientFMTemplateGenerator implements ClientGenerator {
     @Override
     public void generate(Element element, ProcessingEnvironment processingEnv, GafClient gafClientAnnotation) {
 
--- src/main/java/uz/atmos/gaf/client/source_generator/impl/ThriftClientTMTemplateGenerator.java
+++ src/main/java/uz/atmos/gaf/client/source_generator/impl/ThriftClientTMTemplateGenerator.java
@@ -10,7 +10,7 @@ import javax.lang.model.element.Element;
  * Created by Temurbek Ismoilov on 12/03/24.
  */
 
-public class GrpcClientGenerator implements ClientGenerator {
+public class ThriftClientTMTemplateGenerator implements ClientGenerator {
     @Override
     public void generate(Element element, ProcessingEnvironment processingEnv, GafClient gafClientAnnotation) {
 
